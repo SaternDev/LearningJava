943511a49201cb372f173c9661ba3181418fd699
SaternDev <saterndev@gmail.com>
2026-09-18 14:52:34 +0200

    Added Other Tests

---
+++ prueba2.java
@@ -0,0 +1,12 @@
+public class prueba2 {
+    public static void main(String[] args) {
+        int a = 1; int b = 2; int c;
+
+        c = a;
+        a = b;
+        b = c;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
--- pruebas.java
+++ pruebas.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class pruebas {
     public static void main(String[] args) {
         /**
          * Escribir comentario con "/**"
          */
+        //Se detecta el Teclado
+        Scanner lectura = new Scanner(System.in);
+        //Se Serializan las variables
+        double base; double altura;
+
+        System.out.print("Dime la Base");
+        base = lectura.nextDouble();
+
+        System.out.print("Dime la Altura");
+        altura = lectura.nextDouble();
+
+        double area = base * altura;
+        System.out.println("La Area es: " + area);
+
+        lectura.close();
     }
 }
